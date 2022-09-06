32ea0d51f7fbcd7b15cacbd8cd065d9e65547643
Sandeep Mane <sandeepmane1981@gmail.com>
2022-09-06 14:18:54 +0530

    for v2

--- Sources/search.js
+++ Sources/search.js
@@ -9,6 +9,25 @@ function resetSearchInput() {
     });
 }
 
+function showDrinkMenu()
+{
+/*$("d iv.accordion-item.drink").each(function(i) {
+	$(this).show();
+}
+$("div.accordion-item.food").each(function(i) {
+	$(this).hide();
+}	 */
+}
+
+function showFoodMenu()
+{
+	/*$("div.accordion-item.drink").each(function(i) {
+	$(this).hide();
+}
+$("div.accordion-item.food").each(function(i) {
+	$(this).show();
+ }	 */
+}
 
 function chefSpecial() {
     var value = 'chef';
@@ -26,7 +45,9 @@ function chefSpecial() {
         });
 }
 
+
 $(document).ready(function() {
+
 
     $("#searchMenu").on("keyup focus clear", function() {
         var value = $(this).val().toLowerCase();
@@ -36,12 +57,17 @@ $(document).ready(function() {
 
 
         $("div.accordion-item").each(function(i) {
-            if ($(this).height() <= 130) {
+            if ($(this).height() <= 130 && $(this).height() > 60 ) {
                 $(this).hide();
             } else {
-                $(this).show();
+				if ($(this).height() < 60) 
+				{ 
+			//$(this).collapse('toggle'); 
+			}
+                $(this).show(); 
             }
         });
 
     });
 });
+
